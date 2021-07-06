85f049b027fb4492f7257fe41fafe7339933db1f
Shilo <rachel.brain@internode.on.net>
2021-07-06 20:21:44 +1000

    updated css, js & page 3 html code, incl. colors

--- Assets/JS/new.js
+++ Assets/JS/new.js
@@ -1,2 +1,15 @@
 console.log("New branch")
 console.log("Another commit")
+
+
+
+
+// Arrays for 8 colour schemes:
+var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f"];
+var weirdos = ["ac6ab9", "69b00b", "b00b69", "696969", "ab69ba"];
+var summerHarmony = ["00334d", "d82929", "fb8100", "ffc14a", "ece4c0"];
+var deepSeaBaby = ["77d9d9", "77c5d9", "77b2d9", "779ed9", "778bd9"];
+var legacyOfFire = ["f8e6bd", "ffcf00", "f37852", "f93900", "5c2604"];
+var lostRiver = ["133412", "dac1c3", "497529", "5f9422", "7d8078"];
+var othelloCostumes = ["328bc1", "c74e4e", "ffdb6c", "7cd47d", "9c6464"];
+var hubitatDashboards = ["f5f5f5", "0000ff", "ff0000", "077909", "e6e600"];
