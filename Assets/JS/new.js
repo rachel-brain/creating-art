fbdc7b1dc3c688193b416d33a03faddf03ba005d
Shilo <rachel.brain@internode.on.net>
2021-07-07 18:35:57 +1000

    commit with updated page3.html & colors in js

--- Assets/JS/new.js
+++ Assets/JS/new.js
@@ -2,17 +2,6 @@ console.log("New branch")
 console.log("Another commit")
 
 
-// Arrays for 8 colour schemes:
-var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f"];
-var weirdos = ["ac6ab9", "69b00b", "b00b69", "696969", "ab69ba"];
-var summerHarmony = ["00334d", "d82929", "fb8100", "ffc14a", "ece4c0"];
-var deepSeaBaby = ["77d9d9", "77c5d9", "77b2d9", "779ed9", "778bd9"];
-var legacyOfFire = ["f8e6bd", "ffcf00", "f37852", "f93900", "5c2604"];
-var lostRiver = ["133412", "dac1c3", "497529", "5f9422", "7d8078"];
-var othelloCostumes = ["328bc1", "c74e4e", "ffdb6c", "7cd47d", "9c6464"];
-var hubitatDashboards = ["f5f5f5", "0000ff", "ff0000", "077909", "e6e600"];
-
-
 
 // DOM targeting for content injection
 
@@ -31,15 +20,25 @@ var medium = "&medium=wood"
 // Colour Lovers API
 var clUrl = "http://www.colourlovers.com/api/patterns/";
 
-// Rachel please update to include arrays for all your colour schemes //
-var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f"]; // These are the hexadecimal codes without the # symbol
-var weirdos = [1, 2, 3, 4, 5];
-// var nextColorOption = [];
+// Arrays for 8 colour schemes
+var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f"];
+var weirdos = ["ac6ab9", "69b00b", "b00b69", "696969", "ab69ba"];
+var summerHarmony = ["00334d", "d82929", "fb8100", "ffc14a", "ece4c0"];
+var deepSeaBaby = ["77d9d9", "77c5d9", "77b2d9", "779ed9", "778bd9"];
+var legacyOfFire = ["f8e6bd", "ffcf00", "f37852", "f93900", "5c2604"];
+var lostRiver = ["133412", "dac1c3", "497529", "5f9422", "7d8078"];
+var othelloCostumes = ["328bc1", "c74e4e", "ffdb6c", "7cd47d", "9c6464"];
+var hubitatDashboards = ["f5f5f5", "0000ff", "ff0000", "077909", "e6e600"];
 
 var clHEX = [
     dustySunset,
-    "weirdos"
-    // all the others
+    weirdos,
+    summerHarmony,
+    deepSeaBaby,
+    legacyOfFire,
+    lostRiver,
+    othelloCostumes,
+    hubitatDashboards
 ];
 
 // ?? Not sure we need this
--- Assets/JS/testscript.js
+++ Assets/JS/testscript.js
@@ -12,18 +12,34 @@ var imgCheck = "?hasimages=true";
 // Colour Lovers API
 var clUrl = "http://www.colourlovers.com/api/patterns/";
 
-var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f" ];
+// Arrays for 8 colour schemes
+var dustySunset = ["e5834d", "eca265", "fff4e7", "ea8ea1", "d6646f"];
+var weirdos = ["ac6ab9", "69b00b", "b00b69", "696969", "ab69ba"];
+var summerHarmony = ["00334d", "d82929", "fb8100", "ffc14a", "ece4c0"];
+var deepSeaBaby = ["77d9d9", "77c5d9", "77b2d9", "779ed9", "778bd9"];
+var legacyOfFire = ["f8e6bd", "ffcf00", "f37852", "f93900", "5c2604"];
+var lostRiver = ["133412", "dac1c3", "497529", "5f9422", "7d8078"];
+var othelloCostumes = ["328bc1", "c74e4e", "ffdb6c", "7cd47d", "9c6464"];
+var hubitatDashboards = ["f5f5f5", "0000ff", "ff0000", "077909", "e6e600"];
 
 var clHEX = [
-    dustySunset, 
-    "weirdos"
+    dustySunset,
+    weirdos,
+    summerHarmony,
+    deepSeaBaby,
+    legacyOfFire,
+    lostRiver,
+    othelloCostumes,
+    hubitatDashboards
 ];
+
+// ?? Not sure we need this
 var clHexID = [0, 1];
 
 
 // Write to DOM
-function randomResult (objectIDs) {
-    var i = Math.floor(Math.random() * objectIDs.length);   // Pick a random item from the valid results array
+function randomResult(objectIDs) {
+    var i = Math.floor(Math.random() * objectIDs.length); // Pick a random item from the valid results array
 
     // testbench.innerHTML = objectIDs[i];
 
@@ -37,36 +53,38 @@ var col2 = document.getElementById('col2');
 var col3 = document.getElementById('col3');
 var injections = [col1, col2, col3]
 
-function search () {
+function search() {
     fetch("http://www.colourlovers.com/api/patterns/e5834d?format=json")
-    .then(function (response) { return response.json() })
-    .then(function (data) {
-        var fixedData = data.map(function(item) {
-            return {
-                image: item.imageUrl.replace('http', 'https')
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            var fixedData = data.map(function (item) {
+                return {
+                    image: item.imageUrl.replace('http', 'https')
+                }
+            });
+            for (i = 1; i < 4; i++) {
+                injections[i].setAttribute('src', item.i.image);
             }
-        });
-        for (i=1;i<4;i++) {
-            injections[i].setAttribute('src', item.i.image);
-        }
-        console.log(fixedData);
-})
+            console.log(fixedData);
+        })
 }
 
 // Image Retrieval function
-function imgReturn (url) {   // Pass in variables
+function imgReturn(url) { // Pass in variables
 
     fetch(url, {
-        method: 'GET',
-        // mode: 'no-cors',
-    })
-    .then(function (response) {             // Take response and parse it with JSON
-        return response.json();
-    })
-    .then(function (data) {                 // Display primary image result
-        console.log(data);
-        // testbench.innerHTML += "<img src=" + data.imageURL + "></img>"; 
-    })
+            method: 'GET',
+            // mode: 'no-cors',
+        })
+        .then(function (response) { // Take response and parse it with JSON
+            return response.json();
+        })
+        .then(function (data) { // Display primary image result
+            console.log(data);
+            // testbench.innerHTML += "<img src=" + data.imageURL + "></img>"; 
+        })
 }
 
 // // Eventlistener for button press
@@ -78,8 +96,62 @@ function imgReturn (url) {   // Pass in variables
 // })
 
 // Eventlistener for button press
-button.addEventListener("click", function(event) {
-    event.preventDefault();                         // Anonymous function to prevent form reset
+button.addEventListener("click", function (event) {
+    event.preventDefault(); // Anonymous function to prevent form reset
 
-    search();     // Perform a new serach with the chosen paramaters clUrl, dustySunset[0], "?format=json"
+    search(); // Perform a new serach with the chosen paramaters clUrl, dustySunset[0], "?format=json"
 })
+
+
+
+// TODO:
+// HARVARD ART MUSEUMS API data call for page 3
+// Artist name:
+// records[0].people[0].name // eg. Honoré-Victorin Daumier or Lyonel Feininger
+
+// Artist nationality:
+// records[0].people[0].culture // eg. French or American
+
+// Title:
+// records[0].title // eg. Pickhardt Vol. IV: Charivari Lithographs by Daumier or Untitled
+
+// Circa:
+// records[0].dated // eg.1848 or 1940s-1950s
+
+// Technique:
+// records[0].technique // eg. Lithograph or Slide, 35 mm, black and white
+
+// About: Nothing?  May need to remove this section ...
+// ?????
+
+// Image:
+// records[0].images[0].baseimageurl
+// records[0].images[0].width // eg. 774
+// records[0].images[0].height // eg. 1024
+// Or ...
+// records[0].primaryimageurl
+
+
+
+// TODO:
+// MET MUSEUM API data call for page 3
+// Artist name:
+// artistDisplayName // eg. Kiyohara Yukinobu
+
+// Artist nationality:
+// artistNationality // eg. Japanese
+
+// Title:
+// title // eg. Quail and Millet
+
+// Circa:
+// objectDate // eg. late 17th century
+
+// Technique:
+// medium // eg. Hanging scroll; ink and color on silk
+
+// About: Nothing?  May need to remove this section ...
+// ?????
+
+// Image:
+// primaryImage
