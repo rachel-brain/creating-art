3e9d54e59f297911f15e4969c8b6f04cf6754c54
Jesse De Jong <jdoceau@gmail.com>
2021-07-11 12:06:52 +1000

    Intergrated JS with index.html, passing checked options to local storage

--- Assets/JS/page1.js
+++ Assets/JS/page1.js
@@ -8,19 +8,20 @@ if (localStorage.getItem("choices") == null) {                    // Check for l
 // Save checked items to local storage for import to fetch() call
 function saveUserChoices () {
   
-    checkChecks("metChoices");
-    checkChecks("harvardChoices");
-    checkChecks("colorChoices");
-    buttonRedirect();
+    checkChecks("metChecks");
+    checkChecks("harvardChecks");
+    checkChecks("color");
+    // buttonRedirect();
 }
 
 // Check-checks functionality
 function checkChecks (name) {
-
+    console.log(name);
     var checkboxes = document.querySelectorAll("input[name=" + name + "]:checked");     // Check all input tags with <name> for :checked
     var values = [];                                                                    // Establish an array to hold the values
     checkboxes.forEach((checkbox) => {                                                  // Push any :checked <value> information (matching api call vernacular) to the array
         values.push(checkbox.value);
+        console.log(values);
     });
     return localStorage.setItem(name, values);                                          // Push the returned array to local storage for use in the api call on page2
 }
