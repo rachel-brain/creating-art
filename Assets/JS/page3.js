461e5595f35a2956cd44251754ce75b70e307383
Jesse De Jong <jdoceau@gmail.com>
2021-07-11 15:53:04 +1000

    Dynamic image injection for page3 based on user selection implemented, blocked out functionality for object reading and data injection for each api

--- Assets/JS/page3.js
+++ Assets/JS/page3.js
@@ -1,52 +1,55 @@
 // DOM targeting for content injection
 card1 = document.getElementById("chosen-card-1");
-card2 = document.getElementById("chosen-card-2");
+card2 = document.getElementById("chosen-card-2-img");
 
 function colorsWriteObject (focusImage) {
-    // Write object to the DOM  
-        card1.innerHTML += "<h3 class='css'>" + "Accession Year" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.accessionYear + "</p><br>";
-
-        card1.innerHTML += "<h3 class='css'>" + "Credit Line" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.creditLine + "</p><br>";
-
-        card1.innerHTML += "<h3 class='css'>" + "Dimensions" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.dimensions + "</p><br>";
-    }
+// Write object to the DOM  
+    // Extract Image from stored Object
+    card2.setAttribute('src', focusImage.imageUrl.replace('http', 'https'));    // .replace to avoid mixed content error message
+    // Extract Chosen Data from stored Object
+    card1.innerHTML += "<h3 class='css'>" + "Artist" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.username + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Title" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.title + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Date Created" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.dateCreated + "</p><br>";
+}
 
 function harvardWriteObject (focusImage) {
-    // Write object to the DOM  
-        card1.innerHTML += "<h3 class='css'>" + "Accession Year" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.accessionYear + "</p><br>";
-
-        card1.innerHTML += "<h3 class='css'>" + "Credit Line" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.creditLine + "</p><br>";
-
-        card1.innerHTML += "<h3 class='css'>" + "Dimensions" + "</h3><br>";          
-        card1.innerHTML += "<p class='css'>" + focusImage.dimensions + "</p><br>";
-    }
+// Write object to the DOM  
+    // Extract Image from stored Object
+    card2.setAttribute('src', focusImage.records[0].primaryimageurl);   // [0] is static @Chandler please alter this in line with your API amendments
+    // Extract Chosen Data from stored Object
+    card1.innerHTML += "<h3 class='css'>" + "Accession Year" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.accessionYear + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Credit Line" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.creditLine + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Dimensions" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.dimensions + "</p><br>";
+}
 
 function metWriteObject (focusImage) {
-        // Write object to the DOM  
-            card1.innerHTML += "<h3 class='css'>" + "Accession Year" + "</h3><br>";          
-            card1.innerHTML += "<p class='css'>" + focusImage.accessionYear + "</p><br>";
-
-            card1.innerHTML += "<h3 class='css'>" + "Credit Line" + "</h3><br>";          
-            card1.innerHTML += "<p class='css'>" + focusImage.creditLine + "</p><br>";
-
-            card1.innerHTML += "<h3 class='css'>" + "Dimensions" + "</h3><br>";          
-            card1.innerHTML += "<p class='css'>" + focusImage.dimensions + "</p><br>";
-        }
+// Write object to the DOM  
+    // Extract Image from stored Object
+    card2.setAttribute('src', focusImage.primaryImageSmall);
+    // Extract Chosen Data from stored Object
+    card1.innerHTML += "<h3 class='css'>" + "Accession Year" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.accessionYear + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Credit Line" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.creditLine + "</p><br>";
+    card1.innerHTML += "<h3 class='css'>" + "Dimensions" + "</h3><br>";          
+    card1.innerHTML += "<p class='css'>" + focusImage.dimensions + "</p><br>";
+}
 
 function init () {
-    // User Selected
+    // GET User Selected Object
     var focusImage = JSON.parse(localStorage.getItem('focusImage'));
-    // Comparison Variables
+    // Establish Comparison Variables from objects in localstorage
     var focusImageCompare = localStorage.getItem('focusImage');
     var metTargetCompare = localStorage.getItem('metTarget');
     var harvardTargetCompare = localStorage.getItem('harvardTarget');
     var colorsTargetCompare = localStorage.getItem('colorsTarget');
-    // Call the write function for the selected image
+    // Call the write function for the selected object
     if (focusImageCompare === metTargetCompare) {
         metWriteObject(focusImage);
     } else if (focusImageCompare === harvardTargetCompare) {
@@ -56,4 +59,4 @@ function init () {
     }
 }
 
-init();
+init(); // Call on page load
