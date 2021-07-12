196d326ba96a942b0dfe260333dbc2b3e9fb4e93
Afsha <afsha@afterpaytouch.com>
2021-07-12 20:19:34 +1000

    cleanup

--- Assets/JS/home.js
+++ Assets/JS/home.js
@@ -24,3 +24,19 @@ function checkChecks (name) {
     return localStorage.setItem(name, values);                                          // Push the returned array to local storage for use in the api call on page2
 }
 
+// Go Button
+function buttonRedirect() {
+    window.location.href = "searchPage.html";
+  }
+  
+  var goBtn = document.getElementById("goBtn");
+  goBtn.onclick = saveUserChoices;
+  
+  //Function to redirect to history page
+  function redirectHistoryPage(event) {
+    event.preventDefault();
+    window.location.href = "historyPage.html";
+  }
+  
+  //Event listener for redirection to History Page on the click of Search Histroy Button
+  $("#searchHistory").on("click", redirectHistoryPage);
